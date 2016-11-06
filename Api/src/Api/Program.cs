7299ed45c2dfa7da1d8f0e52a5666b8d33b018fd
antur84 <andreas.turku@gmail.com>
2016-11-06 22:58:00 +0100

    Allow change of api port listener from commandline, use dotnet run --urls=http://localhost:52752/

--- Api/src/Api/Program.cs
+++ Api/src/Api/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace Api
 {
@@ -12,7 +13,13 @@ namespace Api
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+               .AddCommandLine(args)
+               .AddJsonFile("hosting.json", optional: true)
+               .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
