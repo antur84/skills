1e61655801d1469a95888ef8710d893b62399612
antur84 <andreas.turku@gmail.com>
2016-11-06 11:23:46 +0100

    spec.

--- ui/src/app/skill-lister/skill-lister.component.spec.ts
+++ ui/src/app/skill-lister/skill-lister.component.spec.ts
@@ -1,19 +1,50 @@
+import { Observable } from 'rxjs/Rx';
+import { SkillService } from './../skill.service';
+import { StarBarComponent } from './../star-bar/star-bar.component';
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
 import { SkillListerComponent } from './skill-lister.component';
+import { MaterialModule } from '@angular/material';
 
 describe('SkillListerComponent', () => {
   let component: SkillListerComponent;
   let fixture: ComponentFixture<SkillListerComponent>;
+  let skillServiceMock: SkillService;
+  let skillViewModels: SkillViewModel[];
+
+  beforeEach(() => {
+    skillViewModels = [
+      {
+        externalId: '1',
+        name: 'js',
+        rating: 1
+      },
+      {
+        externalId: '2',
+        name: 'c#',
+        rating: 5
+      }
+    ];
+  });
+
+  beforeEach(() => {
+    skillServiceMock = jasmine.createSpyObj('skillService', ['getAll']);
+    (skillServiceMock.getAll as jasmine.Spy).and.callFake(() => {
+      return Observable.from([skillViewModels]);
+    });
+  });
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SkillListerComponent ]
+      imports: [MaterialModule.forRoot()],
+      declarations: [SkillListerComponent, StarBarComponent],
+      providers: [
+        { provide: SkillService, useValue: skillServiceMock }
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -24,5 +55,9 @@ describe('SkillListerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should list a line for each skill', () => {
+    expect(fixture.debugElement.queryAll(By.css('app-star-bar')).length).toBe(skillViewModels.length);
   });
 });
